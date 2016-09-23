0899308dac0f0f44ca2212456966f77dc540e7a2
花夏 <liubiao0810@live.cn>
2016-09-23 18:28:10 +0800

    修复选中今天期日choose方法得不到值

--- web/src/app/pages/new/index.js
+++ web/src/app/pages/new/index.js
@@ -123,6 +123,7 @@ module.exports = Vue.extend({
                 istime: true,
                 festival: true, //显示节日
                 choose:   function(datas){
+                    console.log(datas);
                     //选择日期完毕的回调
                     me.poem_time = datas;
                 }
@@ -231,6 +232,7 @@ module.exports = Vue.extend({
                 poem_lines: poem_lines,
                 poem_imgSrc: poem_imgSrc
             };
+            console.log(data);return;
             // 更新
             var _id = this.$route.params.id;
             if (_id) {
