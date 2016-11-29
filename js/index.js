bd0132ca4cb6e4db555977d562ce73d4304467a9
花夏 <liubiao0810@live.cn>
2016-11-29 16:53:50 +0800

    Site updated: 2016-11-29 16:53:50

--- js/index.js
+++ js/index.js
@@ -24081,7 +24081,7 @@
 /* 21 */
 /***/ function(module, exports) {
 
-	module.exports = "<div class=\"ui message\" :class=\"infoData.class\" v-if=\"infoData.state\">\n    <i class=\"close icon\" @click=\"close\"></i>\n    <div class=\"header\" v-if=\"infoData.header\">\n        {{infoData.header}}\n    </div>\n    <p>\n        {{infoData.info}}\n    </p>\n</div>";
+	module.exports = "<div :class=\"[ui, message, infoData.class\" v-if=\"infoData.state\">\n    <i class=\"close icon\" @click=\"close\"></i>\n    <div class=\"header\" v-if=\"infoData.header\">\n        {{infoData.header}}\n    </div>\n    <p>\n        {{infoData.info}}\n    </p>\n</div>";
 
 /***/ },
 /* 22 */
