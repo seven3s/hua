8422b8186ac8d207aa38ba6ea9215fcd784b2709
liubiao0810 <liubiao0810@live.cn>
2016-12-10 19:54:46 +0800

    Site updated: 2016-12-10 19:54:45

--- js/1.1.js
+++ js/1.1.js
@@ -297,7 +297,7 @@ webpackJsonp([1,5],Array(28).concat([
 	            var url = me.$route.path;
 	            var reg = /\/list\//;
 	            var isList = reg.test(url) || (url === '/');
-	            confirm(endTime + ':' + endPoemsTime);
+	            confirm(endTime > endPoemsTime);
 	            if (
 	                util.getScrollTop() + util.getClientHeight() === util.getScrollHeight() && loadMore === 0 && endTime > endPoemsTime && isList) {
 	                confirm(url);
